39f5d330fc08ef80230879a3d345dc70d1d0f931
danbailo <danbailoufms@gmail.com>
2020-04-01 21:21:30 -0400

    1260 solution

--- 1260.cpp
+++ 1260.cpp
@@ -6,45 +6,30 @@ using namespace std;
 
 int main(){
 
-	int n;
+	int n, sum;
 	string tree;
-	map<string, float> map_tree;
-	bool first_occurrence = false;
+	map<string, double> map_tree;
 
 	cin >> n;
-
-	int t = 0;
-	int state;
-	float sum = 0;
-	while(getline(cin, tree)){
-		if(tree != ""){
-			map_tree[tree] += 1;
-		}
-		else{	
-			if(first_occurrence)		{
-				for(map<string, float>::iterator it = map_tree.begin(); it != map_tree.end(); it ++){				
-					sum += it->second;
-				}
-				if(sum != 0){
-					for(auto &m: map_tree){
-						printf("%s %.4f\n", m.first.c_str(), (m.second/sum)*100);
-					}
-				}			
-				map_tree.clear();
-				sum = 0;
-				cout << endl;
+	cin.ignore();
+	getline(cin, tree); //linha em branco.
+	int i = 0;
+	sum = 0;
+	while(i < n){
+		i++;
+		while (getline(cin, tree)){
+			if(tree != ""){
+				map_tree[tree] += 1;
+				sum++;
 			}
-			first_occurrence = true;
-		}		
+			else break;
+		}
+		for(auto &m: map_tree){
+			printf("%s %.4f\n", m.first.c_str(), (m.second/sum) * 100);
+		}
+		map_tree.clear();
+		sum = 0;
+		if(i!=n) printf("\n");
 	}
-	
-	for(map<string, float>::iterator it = map_tree.begin(); it != map_tree.end(); it ++){
-		sum += it->second;
-	}	
-
-	for(auto &m: map_tree){
-		printf("%s %.4f\n", m.first.c_str(), (m.second/sum)*100);
-	}
-
 	return 0;
 }
